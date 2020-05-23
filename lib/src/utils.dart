54344c41a5d803c37ee9ade553fb802dbf12fecc
nisheed.jagadish <nisheed.jagadish@loblaw.ca>
2020-05-22 22:18:02 -0400

    Updated docs for utils.dart file

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,4 +1,4 @@
-// Default letters to fill in the empty spaces
+/// Default letters to fill in the empty spaces
 const String WSLetters = 'abcdefghijklmnoprstuvwy';
 
 /// All the orientations available to build the puzzle
@@ -30,13 +30,51 @@ enum WSOrientation {
 
 /// The puzzle settings intetface
 class WSSettings {
+  /// The recommended height of the puzzle
+  ///
+  /// **Note:** This will automatically increment if
+  /// the words cannot be placed properly in the puzzle
   int height;
+
+  /// The recommended width of the puzzle
+  ///
+  /// **Note:** This will automatically increment if
+  /// the words cannot be placed properly in the puzzle
   int width;
+
+  /// The allowed orientations for the words placed in the puzzle
   final List<WSOrientation> orientations;
+
+  /// The way in which empty spaces in the puzzle should be filled
+  ///
+  /// If `Function` then should return a string with single character
+  /// Example:
+  /// ```
+  /// fillBlanks: () {
+  ///   final fancyStrings = '@#$%^420';
+  ///   return fancyStrings[Random().nextInt(fancyStrings.length)];
+  /// }
+  /// ```
+  ///
+  /// If `String` then should contain a set of unique characters
+  /// Example:
+  /// ```
+  /// fillBlanks: '@#$%^420',
+  /// ```
+  /// If `bool` it will use the default functionality
   final dynamic fillBlanks;
+
+  /// Allow the puzzle to be filled with blanks
   final bool allowExtraBlanks;
+
+  /// Maximum number of attempts to fit the words in the puzzle
   final int maxAttempts;
+
+  /// Maximum numbed of times the grid can grow
+  /// depending on the length of the words and placement
   final int maxGridGrowth;
+
+  /// Allow overlaping of words in the puzzle
   final bool preferOverlap;
 
   WSSettings({
@@ -53,10 +91,19 @@ class WSSettings {
 
 /// Word location interface
 class WSLocation implements WSPosition {
+  /// The column position where the word starts
   final int x;
+
+  /// The row position where the word starts
   final int y;
+
+  /// The orientation the word placed in the puzzle
   final WSOrientation orientation;
+
+  /// The numbed of overlaps the word has
   final int overlap;
+
+  /// The word used
   String word;
 
   WSLocation({
@@ -70,7 +117,10 @@ class WSLocation implements WSPosition {
 
 // Word position interface
 class WSPosition {
+  /// The column position where the word starts
   final int x;
+
+  /// The row position where the word starts
   final int y;
 
   WSPosition({
@@ -81,9 +131,20 @@ class WSPosition {
 
 /// New puzzle interface
 class WSNewPuzzle {
+  /// Two dimentional list containing the puzzle
   List<List<String>> puzzle;
+
+  /// List of word not placed in the puzzle
   List<String> wordsNotPlaced;
+
+  /// List of warnings that occured while creating the puzzle
+  ///
+  /// **Note:** Use this to notify the user of any issues
   List<String> warnings;
+
+  /// List of errors that occured while creating the puzzle
+  ///
+  /// **Note:** Check this before printing or viewing the puzzle
   List<String> errors;
 
   WSNewPuzzle({
@@ -106,12 +167,12 @@ class WSNewPuzzle {
   /// final WSNewPuzzle newPuzzle = wordSearch.newPuzzle(wl, ws);
   /// // Outputs the 2D puzzle
   /// print(newPuzzle.toString());
-  /// ``` 
+  /// ```
   @override
   String toString() {
-    var puzzleString = '';
+    String puzzleString = '';
     for (var i = 0, height = puzzle.length; i < height; i++) {
-      var row = puzzle[i];
+      final List<String> row = puzzle[i];
       for (var j = 0, width = row.length; j < width; j++) {
         puzzleString += (row[j] == '' ? ' ' : row[j]) + ' ';
       }
@@ -123,7 +184,10 @@ class WSNewPuzzle {
 
 /// The solved puzzle interface
 class WSSolved {
+  /// List of words found by solving the puzzle
   List<WSLocation> found;
+
+  /// List of words that were not found while solving the puzzle
   List<String> notFound;
 
   WSSolved({
